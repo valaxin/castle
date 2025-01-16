347b3fc551f6b6d0899a02303d9e7ab02bf2970f
valaxin <15826604+valaxin@users.noreply.github.com>
2025-01-15 20:31:28 -0400

    still borked
    index is nearing, repair page, need social intergration, stylesheet cleanup needed

--- library/data/repositories.js
+++ library/data/repositories.js
@@ -2,7 +2,8 @@ import 'dotenv/config'
 
 export default async function GitHubRepositories() {
   try {
-    const url = `https://api.github.com/users/${process.env.GITHUB_USERNAME}/repos`
+    const url = `https://api.github.com/users/${process.env.USERNAME}/repos`
+    console.log("!!!!", url)
     const response = await fetch(url, { method: 'GET' })
     const resp = await response.text()
     const json = JSON.parse(resp)
