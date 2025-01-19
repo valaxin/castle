4c36a73a4e6e64b78498219907338bb52080cb39
valaxin <15826604+valaxin@users.noreply.github.com>
2025-01-18 20:52:54 -0400

    index changes, likely still borked

--- app/modules/class-injections.js
+++ app/modules/class-injections.js
@@ -1,16 +1,18 @@
 'use strict'
 
 // adds 'bulma' classes to custom containers within a given blog post
-
 function articleContainers(articleSelector) {
 
   try {
     let elements = ''
     const elementTypes = ['spoilers', 'warning', 'information']
     const articleBody = document.body.querySelectorAll(articleSelector)[0]
+    if (!articleBody) { 
+      console.log('no article')
+      return false
+    }
     for (const type of elementTypes) {
       elements = articleBody.querySelectorAll(`.${type}`)
-      console.log(elements)
       for (const el of elements) {
         const header = `<div class="message-header"><p>${type}</p></div>`
         el.children[0].classList.add('message-body')
@@ -24,13 +26,31 @@ function articleContainers(articleSelector) {
   }
 }
 
+// add highlighting classes to current page within navigation elements
+function navHighlighting(navigationSelector) {
+  let navigationLinks = document.body.querySelectorAll(navigationSelector)
+  console.log(window.location.pathname, navigationLinks)
+
+  for (link of navigationsLinks) {
+    
+  }
+}
+
 // add bulma classes when viewport reaches a defined width size
 
 export default (async function () {
-  if (window.location.pathname.includes('/blog/')) {
+  let pathSlug = 'blog'
+  if (window.location.pathname.includes(`/${pathSlug}`)) {
+    
     const containers = articleContainers('section.content > article.content')
     if (containers) {
       console.log('custom-containers:', containers)
     }
   }
+
+  const links = navHighlighting('nav.navbar > div a')
+  if (links) {
+    console.log('navigation-links', links)
+  }
+
 })()
