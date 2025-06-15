3f40b1a834840e86b68101c1ff3086da84468a5f
valaxin <15826604+valaxin@users.noreply.github.com>
2025-06-15 10:24:43 -0300

    checkin

--- src/javascript/modules/color-scheme.js
+++ src/javascript/modules/color-scheme.js
@@ -1,41 +1,38 @@
-// what? find the button?
-
-// it should be something like?
-
 export default (async () => {
-
   const scheme = document.querySelector('html')
   const button = document.querySelector('nav.navbar button[data-colorpref]')
-  const faicon = button.querySelector('i.fa')
+  const innerIcon = button.querySelector('i.fa')
 
-  
-  const modifyClass = (e, icon) => {
-    console.log(e, icon)
+  const switchIconTo = (newIcon) => {
+    let lastClass = innerIcon.classList[innerIcon.classList.length - 1]
+    innerIcon.classList.replace(lastClass, `fa-${newIcon}`)
   }
   
   if (localStorage) {
-
-    // check in local storage if there set into dom before eval
     const theme = localStorage.getItem('castle-theme')
     if (localStorage.getItem('castle-theme')) {
       scheme.dataset.theme = theme
     }
     
-    // add event to button and eval on click
     button.addEventListener('click', (e) => {
       switch (scheme.dataset.theme) {
+
         case 'light':
           scheme.dataset.theme = 'dark'
-          localStorage.setItem('castle-theme', 'dark')
+          switchIconTo('moon')
+          localStorage.setItem('castle-theme', scheme.dataset.theme)
           break
+  
         case 'dark':
           scheme.dataset.theme = 'auto'
-          localStorage.setItem('castle-theme', 'auto')
+          switchIconTo('desktop')
+          localStorage.setItem('castle-theme', scheme.dataset.theme)
           break
+
         case 'auto':
           scheme.dataset.theme = 'light'
-          modifyClass('dsds', 'sun')
-          localStorage.setItem('castle-theme', 'light')
+          switchIconTo('sun')
+          localStorage.setItem('castle-theme', scheme.dataset.theme)
       }
     })
   }
